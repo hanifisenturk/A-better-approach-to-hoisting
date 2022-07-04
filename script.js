1e27c7cc5642113933ea926d4ce6571939309d94
Mehmet Hanifi Senturk <mehmetsenturk@protonmail.com>
2022-07-04 17:08:22 +0300

    The codes that we will talk abouthave been uploaded.

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log(a); // Expected output: undefined
+
+var a = "Hello World";
+
+//Function declaration
+
+myFunction(); // Expected output: "This is a function declaration"
+
+function myFunction() {
+  console.log("This is a function declaration");
+}
+
+// Function expression
+
+console.log(typeof yourFunction); // Expected output: undefined.
+
+yourFunction(); // Expected output: your function is not a function;
+
+var yourFunction = function () {
+  console.log("This is a function expression");
+};
